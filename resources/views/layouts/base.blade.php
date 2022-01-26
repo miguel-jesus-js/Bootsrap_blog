c693b51967f6e262d40a134f3fe83904379d6fdf
Wilberth <wilberthlp4@gmail.com>
2022-01-25 19:18:40 -0600

    Implementación del menú

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,37 +4,69 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>BOOTSTRAP BLOG</title>
+  <link rel="stylesheet" href={{ URL::asset("dist/css/estiloGeneral.css")}}>
   <link rel="stylesheet" href={{ URL::asset("dist/css/tabler.min.css")}}>
   <link rel="stylesheet" href={{ URL::asset("dist/css/demo.min.css")}}>
   <link rel="stylesheet" href={{ URL::asset("iconfont/tabler-icons.min.css")}}>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Bootstrap</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/post">Post</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/create_post">Craer post</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/messages">Mensajes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/view_post">Ver post</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+
+  <header class="navbar navbar-expand-md navbar-dark d-print-none">
+        <div class="container-xl">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+              <a href=".">
+                BootstrapBlog
+              </a>
+            </h1>
+            
+
+            <div class="collapse navbar-collapse" id="navbar-menu">
+              <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center justify-content-end">
+                <ul class="navbar-nav" id="menuBoot">
+                  <li class="nav-item {{ (request()->is('/')) ? 'active' : '' }}">
+                    <a class="nav-link" href="/">
+                      <span class="nav-link-title">
+                        Home
+                      </span>
+                    </a>
+                  </li>
+                  <li class="nav-item {{ (request()->is('post')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('post') }}">
+                      <span class="nav-link-title">
+                        Post
+                      </span>
+                    </a>
+                  </li>
+                  <li class="nav-item {{ (request()->is('create_post')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('create_post') }}">
+                      <span class="nav-link-title">
+                        Crear Post
+                      </span>
+                    </a>
+                  </li>
+                  <li class="nav-item {{ (request()->is('view_post')) ? 'active' : ''}}">
+                    <a class="nav-link" href="{{ url('view_post') }}">
+                      <span class="nav-link-title">
+                        Ver Post
+                      </span>
+                    </a>
+                  </li>
+                  <li class="nav-item {{ (request()->is('messages')) ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ url('messages') }}">
+                      <span class="nav-link-title">
+                        Mensajes
+                      </span>
+                      <span class="badge bg-red badge-item">2</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </div>
+  </header>
     @yield('contenido')
     <script src={{ URL::asset("dist/js/jquery.min.js")}}></script>
     <script src={{ URL::asset("dist/js/tabler.min.js")}}></script>
